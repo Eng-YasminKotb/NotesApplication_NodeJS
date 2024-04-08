3cd4349fc6c4071901bc597bd6e6a286241f3fbf
Yasmin <Yasmin.Kotb21@gmail.com>
2024-04-08 23:40:42 +0200

    update_method

--- controller/noteController.js
+++ controller/noteController.js
@@ -5,18 +5,18 @@ var memstorage=require('../Util/memory.storage')
 var model=require('../model/note.model')
 
 exports.getAllNotes= function(req,res) {
-   /*  var sqlId_1=generator.generate();
-    memsrorage.store.setItem(sqlId_1, "1st_key")
-    var sqlId_2= generator.generate();
-    memsrorage.store.setItem(sqlId_2, "2nd_key")
+   // var sqlId_1=generator.generate();
+    //memstorage.store.setItem(sqlId_1, "1st_key")
+    //var sqlId_2= generator.generate();
+  // memstorage.store.setItem(sqlId_2, "2nd_key")
 
-   var keys= memsrorage.getKeys(memsrorage.store);
-   var values=memsrorage.getValues(memsrorage.store)
-    console.log("values........."+JSON.stringify(values))
+   var keys= memstorage.getKeys(memstorage.store);
+   var values=memstorage.getValues(memstorage.store)
+   console.log("values........."+JSON.stringify(values));
     
-    var Note=model.Note;
-    var noteObj=new Note(sqlId_1,"Best than yester day","keep going under any presure","Yasmin",new Date()); */
-    res.send("get all notes By yasmina");
+   // var Note=model.Note;
+  //  var noteObj=new Note(sqlId_1,"Best than yester day","keep going under any presure","Yasmin",new Date()); */
+    return res.status(200).send(JSON.stringify(values));
 
 
 }
@@ -43,7 +43,25 @@ exports.saveNotes= function(req,res) {
 }
 
 exports.UpdateNotes= function(req,res) {
-    res.send("Update notes.....");
+    
+    var Create_By="Yasmin Kotb";
+    var Create_At=new Date();
+
+    //req.body
+    var noteId=req.body.noteId;
+    var title=req.body.title;
+    var content=req.body.content;
+    
+    if(!noteId) return res.status(500).send({error: 'NoteID shoud not be empty!'})
+    if(!title  || !content)   return res.status(500).send({error: 'Title  Content shoud not be empty'})
+    
+
+    var noteItem=memstorage.store.getItem(noteId)//getItem return null
+    if(!noteItem) return res.status(500).send({error: 'Failed to find note! Please check the Id of the note'})
+        var Note=model.Note;
+        var noteObj=new Note(noteId,title,content,Create_By,Create_At);
+        memstorage.store.setItem(noteId, noteObj);
+        res.send("Successfully note is updates!");
 }
 
 exports.deleteNotes= function(req,res) {
